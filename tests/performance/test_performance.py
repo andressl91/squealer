d0ff0a5a3fb99ab3497cd6b2fda982b046ad24fe
andressl91 <pep8@pm.me>
2019-02-01 15:55:06 +0100

    Solved tempfile with tempdir

--- tests/performance/test_performance.py
+++ tests/performance/test_performance.py
@@ -1,6 +1,9 @@
 import pytest 
 import time
 import tempfile
+
+from pathlib import Path
+
 from squealer.sql_table_tools import DataTableTools
 from squealer.sqlite_session import SqliteSession
 
@@ -14,79 +17,87 @@ def get_lots_of_data(keys, n_rows):
     return sql_data
 
 
-#Old scool
 def test_read_and_write_table():
 
-    with tempfile.TemporaryFile(suffix=".db", prefix="test") as tf:
-        tf = str(tf)
-        db_tools = DataTableTools(db_path=tf)
-        keys = ["a", "b", "c", "d", "e"]
-        cat = ["INTEGER" for i in range(len(keys))]
-        categories = dict(zip(keys, cat))
-        db_tools.create_table(table_name="data",
-                              categories=categories)
+    #tf = tempfile.NamedTemporaryFile(suffix=".db", prefix="test", delete=True)
+    #tfile, tdir  = tempfile.mkstemp(suffix=".db", prefix="test")
+    td = tempfile.mkdtemp()
+    
+    tf = Path(td) / "test.db"
+    #tf1 = str(tdir) #Path(str(tf))
 
-        # Test write, and order of write features is arbritary
-        data_table = db_tools.tables["data"]
-        n_rows = 1000
-        sql_data = get_lots_of_data(keys, n_rows=n_rows)
+    db_tools = DataTableTools(db_path=tf)
+    keys = ["a", "b", "c", "d", "e"]
+    cat = ["INTEGER" for i in range(len(keys))]
+    categories = dict(zip(keys, cat))
+    db_tools.create_table(table_name="data",
+                          categories=categories)
 
-        t_serial_write = time.time()
-        for data in sql_data:
-            data_table.write(data)
-        t_serial_write_end = time.time() - t_serial_write
+    # Test write, and order of write features is arbritary
+    data_table = db_tools.tables["data"]
+    n_rows = 1000
+    sql_data = get_lots_of_data(keys, n_rows=n_rows)
 
-        t_multi_write = time.time()
-        data_table.multi_write(sql_data)
-        t_multi_write_end = time.time() - t_multi_write
+    t_serial_write = time.time()
+    for data in sql_data:
+        data_table.write(data)
+    t_serial_write_end = time.time() - t_serial_write
+
+    t_multi_write = time.time()
+    data_table.multi_write(sql_data)
+    t_multi_write_end = time.time() - t_multi_write
 
 
-        factor = t_multi_write_end/t_serial_write_end
-        print(f"Using {len(categories)} categories with {n_rows} rows.")
-        print(f"Single write uses {t_serial_write_end}",
-              f"Multi write uses {t_multi_write_end}")
-        print(f"Multi write is faster/slower with a factor of {factor}")
+    factor = t_multi_write_end/t_serial_write_end
+    print(f"Using {len(categories)} categories with {n_rows} rows.")
+    print(f"Single write uses {t_serial_write_end}",
+          f"Multi write uses {t_multi_write_end}")
+    print(f"Multi write is faster/slower with a factor of {factor}")
 
+    
+    #td.close()
 
 
 def single_write_of_rows(n_rows):
-    with tempfile.NamedTemporaryFile(suffix=".db", prefix="test") as tf:
-        tf = str(tf)
-        db_tools = DataTableTools(db_path=tf)
-        keys = ["a", "b", "c", "d", "e"]
-        cat = ["INTEGER" for i in range(len(keys))]
-        categories = dict(zip(keys, cat))
-        db_tools.create_table(table_name="data",
-                              categories=categories)
+    
+    td = tempfile.mkdtemp()
+    tf = Path(td) / "test.db"
+    tf = str(tf)
+    db_tools = DataTableTools(db_path=tf)
+    keys = ["a", "b", "c", "d", "e"]
+    cat = ["INTEGER" for i in range(len(keys))]
+    categories = dict(zip(keys, cat))
+    db_tools.create_table(table_name="data",
+                          categories=categories)
 
-        data_table = db_tools.tables["data"]
-        sql_data = get_lots_of_data(keys, n_rows=n_rows)
+    data_table = db_tools.tables["data"]
+    sql_data = get_lots_of_data(keys, n_rows=n_rows)
 
-        for data in sql_data:
-            data_table.write(data)
+    for data in sql_data:
+        data_table.write(data)
 
-        return len(sql_data)
+    return len(sql_data)
 
 
 
 def multiple_write_of_rows(n_rows):
-    with tempfile.NamedTemporaryFile(suffix=".db", prefix="test") as tf:
-        tf = str(tf)
-        db_tools = DataTableTools(db_path=tf)
-        keys = ["a", "b", "c", "d", "e"]
-        cat = ["INTEGER" for i in range(len(keys))]
-        categories = dict(zip(keys, cat))
-        db_tools.create_table(table_name="data",
-                              categories=categories)
+    td = tempfile.mkdtemp()
+    tf = Path(td) / "test.db"
+    tf = str(tf)
+    db_tools = DataTableTools(db_path=tf)
+    keys = ["a", "b", "c", "d", "e"]
+    cat = ["INTEGER" for i in range(len(keys))]
+    categories = dict(zip(keys, cat))
+    db_tools.create_table(table_name="data",
+                          categories=categories)
 
-        data_table = db_tools.tables["data"]
-        sql_data = get_lots_of_data(keys, n_rows=n_rows)
+    data_table = db_tools.tables["data"]
+    sql_data = get_lots_of_data(keys, n_rows=n_rows)
 
-        data_table.multi_write(sql_data)
+    data_table.multi_write(sql_data)
 
-        return len(sql_data)
+    return len(sql_data)
 
-@pytest.mark.skip
 def test_benchmark_single_row_write(benchmark):
     n_rows = 1000
     in_kwargs = {"n_rows": n_rows} 
@@ -97,7 +108,6 @@ def test_benchmark_single_row_write(benchmark):
     assert result == n_rows
 
 
-@pytest.mark.skip
 def test_benchmark_multiple_row_write(benchmark):
     n_rows = 1000
     in_kwargs = {"n_rows": n_rows} 
